38212bd3c6b738ef29ceb7d0c2e55875b8ad6e4c
Unknown <antoine.carlier@isen.yncrea.fr>
2019-10-29 12:38:34 +0100

    Printable Text

--- src/game.py
+++ src/game.py
@@ -140,6 +140,7 @@ class Game:
                         elif event.key == pygame.K_ESCAPE:
                             # On quitte l'inventaire
                             game_area = 0
+                            hud_cursor = 1
 
                         elif event.key == pygame.K_UP:  # Fleche du haut
                             if inventory_cursor[1] > 0:
