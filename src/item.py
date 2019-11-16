8f08928681f8bc8822d3f9850fac531a7104b9d5
kaetir <thibault.desprez@isen.yncrea.fr>
2019-11-16 10:00:12 +0100

    ousp tab

--- src/item.py
+++ src/item.py
@@ -20,335 +20,335 @@ from src.perso.character import Character
 
 
 class Item:
-	def __init__(self):
-		self.name = ""
-		self.item_type = ""
-		self.icon_id = 0
-		self.prix = randint(1, 42)
+    def __init__(self):
+        self.name = ""
+        self.item_type = ""
+        self.icon_id = 0
+        self.prix = randint(1, 42)
 
-	def get_icon_id(self):
-		return self.icon_id
+    def get_icon_id(self):
+        return self.icon_id
 
-	def set_icon_id(self, icon_id):
-		self.icon_id = icon_id
+    def set_icon_id(self, icon_id):
+        self.icon_id = icon_id
 
-	@staticmethod
-	def random():
-		# incrémenter le deuxieme paramettre pour ajouter des catégories
-		typ = randint(1, 4)
-		if typ == 1:
-			# equipement
-			return Equipment()
-		elif typ == 2:
-			# consumable
-			return Consumables()
-		elif typ == 3:
-			# speelbook
-			return SpellBook()
+    @staticmethod
+    def random():
+        # incrémenter le deuxieme paramettre pour ajouter des catégories
+        typ = randint(1, 4)
+        if typ == 1:
+            # equipement
+            return Equipment()
+        elif typ == 2:
+            # consumable
+            return Consumables()
+        elif typ == 3:
+            # speelbook
+            return SpellBook()
 
 
 class Equipment(Item):
 
-	def __init__(self) -> None:
-		super().__init__()
+    def __init__(self) -> None:
+        super().__init__()
 
 
 class Weapon(Equipment):
-	""" emplacement : 
+    """ emplacement :
 	weapon, shield
 	"""
 
-	static_equipment_type = ["weapon", "shield"]
+    static_equipment_type = ["weapon", "shield"]
 
-	swordDamage = {
-		10: 3,
-		11: 3,
-		13: 3,
-		12: 3,
-		14: 3,
-		15: 3,
-		16: 3,
-		17: 6,
-		20: 3,
-		21: 3,
-		22: 3,
-		24: 3,
-		23: 3,
-		25: 3,
-		26: 3,
-		27: 6,
-		30: 3,
-		31: 3,
-		32: 3,
-		33: 3,
-		34: 6,
-		35: 8,
-		36: 3,
-		37: 3,
-		78: 15
-	}
+    swordDamage = {
+        10: 3,
+        11: 3,
+        13: 3,
+        12: 3,
+        14: 3,
+        15: 3,
+        16: 3,
+        17: 6,
+        20: 3,
+        21: 3,
+        22: 3,
+        24: 3,
+        23: 3,
+        25: 3,
+        26: 3,
+        27: 6,
+        30: 3,
+        31: 3,
+        32: 3,
+        33: 3,
+        34: 6,
+        35: 8,
+        36: 3,
+        37: 3,
+        78: 15
+    }
 
-	def __init__(self, type_equip: str = None) -> None:
-		super().__init__()
-		if type_equip is None:
-			self.equipment_type = choice(self.static_equipment_type)
-		else:
-			if type_equip in self.static_equipment_type:
-				self.equipment_type = type_equip
-			else:
-				self.equipment_type = choice(self.static_equipment_type)
+    def __init__(self, type_equip: str = None) -> None:
+        super().__init__()
+        if type_equip is None:
+            self.equipment_type = choice(self.static_equipment_type)
+        else:
+            if type_equip in self.static_equipment_type:
+                self.equipment_type = type_equip
+            else:
+                self.equipment_type = choice(self.static_equipment_type)
 
-		self.random_icon()
+        self.random_icon()
 
-		if self.equipment_type == "weapon":
-			self.prix = self.swordDamage[self.icon_id] * 100
-			# 5% chance of parry
-			self.block_chance = 0.05
-			self.damage = self.swordDamage[self.icon_id]
-		else:
-			self.prix = 100
-			self.damage = 1
-			# 30% chance of parry
-			self.block_chance = 0.3
+        if self.equipment_type == "weapon":
+            self.prix = self.swordDamage[self.icon_id] * 100
+            # 5% chance of parry
+            self.block_chance = 0.05
+            self.damage = self.swordDamage[self.icon_id]
+        else:
+            self.prix = 100
+            self.damage = 1
+            # 30% chance of parry
+            self.block_chance = 0.3
 
-		self.precision = 0.95
+        self.precision = 0.95
 
-	def random_icon(self):
-		if self.equipment_type == "weapon":
-			self.icon_id = choice(items_id["swords"])
-		elif self.equipment_type == "shield":
-			self.icon_id = choice(items_id["shields"])
+    def random_icon(self):
+        if self.equipment_type == "weapon":
+            self.icon_id = choice(items_id["swords"])
+        elif self.equipment_type == "shield":
+            self.icon_id = choice(items_id["shields"])
 
-	def hit(self) -> int:
-		if random() > self.precision:
-			return self.damage
-		return 0
+    def hit(self) -> int:
+        if random() > self.precision:
+            return self.damage
+        return 0
 
 
 class Armor(Equipment):
-	item_type = "armor"
-	""" emplacement : 
-	helmet, chest, legs, boots, gloves
-	"""
+    item_type = "armor"
+    """ emplacement : 
+    helmet, chest, legs, boots, gloves
+    """
 
-	static_equipment_type = ["helmet", "chest", "legs", "boots", "gloves", "amulet", "ring"]
+    static_equipment_type = ["helmet", "chest", "legs", "boots", "gloves", "amulet", "ring"]
 
-	costs = {
-		"helmets": {
-			40 : 0,
-			45 : 0,
-			50 : 0,
-			55 : 0,
-			60 : 0,
-			65 : 0
-		},
-		"chests": {
-			41 : 0,
-			46 : 0,
-			51 : 0,
-			56 : 0,
-			61 : 0,
-			66 : 0
-		},
-		"legs": {
-			42 : 0,
-			47 : 0,
-			52 : 0,
-			57 : 0,
-			62 : 0,
-			67 : 0
-		},
-		"boots": {
-			43 : 0,
-			48 : 0,
-		 	53 : 0,
-			58 : 0,
-			63 : 0,
-			68 : 0
-		},
-		"gloves": {
-			44 : 0,
-			49 : 0,
-			54 : 0,
-			59 : 0,
-			64 : 0,
-			69 : 0
-		}
-	}
+    value = {
+        "helmets": {
+            40: 0,
+            45: 0,
+            50: 0,
+            55: 0,
+            60: 0,
+            65: 0
+        },
+        "chests": {
+            41: 0,
+            46: 0,
+            51: 0,
+            56: 0,
+            61: 0,
+            66: 0
+        },
+        "legs": {
+            42: 0,
+            47: 0,
+            52: 0,
+            57: 0,
+            62: 0,
+            67: 0
+        },
+        "boots": {
+            43: 0,
+            48: 0,
+            53: 0,
+            58: 0,
+            63: 0,
+            68: 0
+        },
+        "gloves": {
+            44: 0,
+            49: 0,
+            54: 0,
+            59: 0,
+            64: 0,
+            69: 0
+        }
+    }
 
-	costsGold = {
-		 "helmets": {
-			40 :0,
-			45 :0,
-			50 :0,
-			55 :0,
-			60 :0,
-			65 :0
-   		},
-		"chests": {
-			41 : 0,
-			46 : 0,
-			51 : 0,
-			56 : 0,
-			61 : 0,
-			66 : 0
-		},
-		"legs": {
-			42 : 0,
-			47 : 0,
-			52 : 0,
-			57 : 0,
-			62 : 0,
-			67 : 0
-		},
-		"boots": {
-			43 : 0,
-			48 : 0,
-			53 : 0,
-			58 : 0,
-			63 : 0,
-			68 : 0
-		},
-		"gloves": {
-			44 : 0,
-			49 : 0,
-			54 : 0,
-			59 : 0,
-			64 : 0,
-			69 : 0
-		}
-	}
+    costsGold = {
+        "helmets": {
+            40: 0,
+            45: 0,
+            50: 0,
+            55: 0,
+            60: 0,
+            65: 0
+        },
+        "chests": {
+            41: 0,
+            46: 0,
+            51: 0,
+            56: 0,
+            61: 0,
+            66: 0
+        },
+        "legs": {
+            42: 0,
+            47: 0,
+            52: 0,
+            57: 0,
+            62: 0,
+            67: 0
+        },
+        "boots": {
+            43: 0,
+            48: 0,
+            53: 0,
+            58: 0,
+            63: 0,
+            68: 0
+        },
+        "gloves": {
+            44: 0,
+            49: 0,
+            54: 0,
+            59: 0,
+            64: 0,
+            69: 0
+        }
+    }
 
-	def __init__(self) -> None:
-		super().__init__()
-		self.equipment_type = choice(self.static_equipment_type)
-		self.prix = 1
-		self.value = 1
-		self.random_equipement()
+    def __init__(self) -> None:
+        super().__init__()
+        self.equipment_type = choice(self.static_equipment_type)
+        self.prix = 1
+        self.value = 1
+        self.random_equipement()
 
-	def random_equipement(self):
-		if self.equipment_type == "helmet":
-			self.icon_id = choice(items_id["helmets"])
-			self.value = 2
-			self.prix = randint(150, 200)
-		elif self.equipment_type == "chest":
-			self.icon_id = choice(items_id["chests"])
-			self.value = 5
-			self.prix = randint(500, 1000)
-		elif self.equipment_type == "legs":
-			self.icon_id = choice(items_id["legs"])
-			self.value = 3
-			self.prix = randint(300, 500)
-		elif self.equipment_type == "boots":
-			self.icon_id = choice(items_id["boots"])
-			self.value = 2
-			self.prix = randint(50, 100)
-		elif self.equipment_type == "gloves":
-			self.icon_id = choice(items_id["gloves"])
-			self.value = 2
-			self.prix = randint(50, 100)
-		elif self.equipment_type == "amulet":
-			self.icon_id = choice(items_id["amulets"])
-			self.value = 1
-			self.prix = randint(50, 100)
-		elif self.equipment_type == "ring":
-			self.icon_id = choice(items_id["rings"])
-			self.value = 1
-			self.prix = randint(50, 100)
+    def random_equipement(self):
+        if self.equipment_type == "helmet":
+            self.icon_id = choice(items_id["helmets"])
+            self.value = 2
+            self.prix = randint(150, 200)
+        elif self.equipment_type == "chest":
+            self.icon_id = choice(items_id["chests"])
+            self.value = 5
+            self.prix = randint(500, 1000)
+        elif self.equipment_type == "legs":
+            self.icon_id = choice(items_id["legs"])
+            self.value = 3
+            self.prix = randint(300, 500)
+        elif self.equipment_type == "boots":
+            self.icon_id = choice(items_id["boots"])
+            self.value = 2
+            self.prix = randint(50, 100)
+        elif self.equipment_type == "gloves":
+            self.icon_id = choice(items_id["gloves"])
+            self.value = 2
+            self.prix = randint(50, 100)
+        elif self.equipment_type == "amulet":
+            self.icon_id = choice(items_id["amulets"])
+            self.value = 1
+            self.prix = randint(50, 100)
+        elif self.equipment_type == "ring":
+            self.icon_id = choice(items_id["rings"])
+            self.value = 1
+            self.prix = randint(50, 100)
 
 
 class Consumables(Item):
-	item_type = "consumable"
-	bonus: bool = True
+    item_type = "consumable"
+    bonus: bool = True
 
-	def __init__(self):
-		super().__init__()
-		self.icon_id = choice(items_id["sandwichs"])
+    def __init__(self):
+        super().__init__()
+        self.icon_id = choice(items_id["sandwichs"])
 
-	def use(self, c: Character) -> None:
-		if self.bonus:
-			c.heal(c.max_health // 2)
-			c.mana += 3 if c.mana + 3 <= c.max_mana else c.max_mana - c.mana
+    def use(self, c: Character) -> None:
+        if self.bonus:
+            c.heal(c.max_health // 2)
+            c.mana += 3 if c.mana + 3 <= c.max_mana else c.max_mana - c.mana
 
-		print("Yummmy !!!!🥪")
+        print("Yummmy !!!!🥪")
 
 
 class SpellBook(Item):
-	item_type = "spellbook"
-	bonus: bool = False
+    item_type = "spellbook"
+    bonus: bool = False
 
-	IsBonus = {
-		70: False,  # poison
-		71: False,  # boom
-		72: False,  # skip
-		73: True,  # armorBoost
-		74: True,  # heal
-		75: True  # vol a la tire
-	}
-	costs = {
-		70: 1,  # poison
-		71: 1,  # boom
-		72: 0,  # skip
-		73: 5,  # armorBoost
-		74: 2,  # heal
-		75: 1  # vol a la tire
-	}
+    IsBonus = {
+        70: False,  # poison
+        71: False,  # boom
+        72: False,  # skip
+        73: True,  # armorBoost
+        74: True,  # heal
+        75: True  # vol a la tire
+    }
+    costs = {
+        70: 1,  # poison
+        71: 1,  # boom
+        72: 0,  # skip
+        73: 5,  # armorBoost
+        74: 2,  # heal
+        75: 1  # vol a la tire
+    }
 
-	costsGold = {
-		70: [150, 300],  # poison
-		71: [300, 1000],  # boom
-		72: [1, 1],      # skip
-		73: [100, 200],  # armorBoost
-		74: [200, 400],  # heal
-		75: [200, 400]  # vol a la tire
-	}
+    costsGold = {
+        70: [150, 300],  # poison
+        71: [300, 1000],  # boom
+        72: [1, 1],  # skip
+        73: [100, 200],  # armorBoost
+        74: [200, 400],  # heal
+        75: [200, 400]  # vol a la tire
+    }
 
-	def __init__(self):
-		super().__init__()
-		self.random_spellbook()
-		self.cost = self.costs[self.icon_id]
-		self.bonus = self.IsBonus[self.icon_id]
-		self.prix = randint(self.costsGold[self.icon_id][0], self.costsGold[self.icon_id][1])
+    def __init__(self):
+        super().__init__()
+        self.random_spellbook()
+        self.cost = self.costs[self.icon_id]
+        self.bonus = self.IsBonus[self.icon_id]
+        self.prix = randint(self.costsGold[self.icon_id][0], self.costsGold[self.icon_id][1])
 
-	def use(self, source: Character, destination: Monster) -> bool:
-		"""
+    def use(self, source: Character, destination: Monster) -> bool:
+        """
 		@summary utilise un spell de la source sur la destination si c'est un monstre et que le sort est un debuff
 		@param source: le joueur qui lance le sort
 		@param destination: le mob qui prend cher
 		@return: si le sort a pu etre lancé
 		"""
-		if self.cost > source.mana:
-			return False
-		source.mana -= self.cost
+        if self.cost > source.mana:
+            return False
+        source.mana -= self.cost
 
-		if self.bonus:
-			# heal
-			if self.icon_id == 74:
-				source.health = source.max_health
-			# shield
-			elif self.icon_id == 73:
-				source.armor += 5
-			# vol a la tire
-			elif self.icon_id == 75:
-				source.inventory.money += randint(25, 50)
-				destination.take_damage(destination.max_health // 5)
-			print("mathémagie BONUS", self.icon_id)
-			print("{} use a spell on himself".format(source.name))
-		else:
-			# TODO states -> poison
-			if self.icon_id == 70:
-				destination.take_damage(destination.max_health // 3)
-			# Boom == massdamage
-			if self.icon_id == 71:
-				destination.health -= source.mana * 3
-				source.mana = 0
-			# TODO skip turn
-			if self.icon_id == 72:
-				print("POUF")
-			print("mathémagie MALLUS", self.icon_id)
-			print("{} use a spell on {}".format(self.name, destination.name))
+        if self.bonus:
+            # heal
+            if self.icon_id == 74:
+                source.health = source.max_health
+            # shield
+            elif self.icon_id == 73:
+                source.armor += 5
+            # vol a la tire
+            elif self.icon_id == 75:
+                source.inventory.money += randint(25, 50)
+                destination.take_damage(destination.max_health // 5)
+            print("mathémagie BONUS", self.icon_id)
+            print("{} use a spell on himself".format(source.name))
+        else:
+            # TODO states -> poison
+            if self.icon_id == 70:
+                destination.take_damage(destination.max_health // 3)
+            # Boom == massdamage
+            if self.icon_id == 71:
+                destination.health -= source.mana * 3
+                source.mana = 0
+            # TODO skip turn
+            if self.icon_id == 72:
+                print("POUF")
+            print("mathémagie MALLUS", self.icon_id)
+            print("{} use a spell on {}".format(self.name, destination.name))
 
-		return True
+        return True
 
-	def random_spellbook(self):
-		self.icon_id = choice(items_id["spellbooks"])
+    def random_spellbook(self):
+        self.icon_id = choice(items_id["spellbooks"])
